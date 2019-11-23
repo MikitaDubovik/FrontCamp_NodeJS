c7a1ee39a322c1c822b4c237433fa22e052ad83b
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-11-23 18:22:42 +0300

    Task 2 and

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const LoggerProxy = require('./scripts/logger-proxy/logger-proxy').LoggerProxy;
 
 var loggerProxy = new LoggerProxy(new HttpClient());
 
-app.get('/news', (req, res) => {
-    res.send(loggerProxy.get())
+app.get('/news', async (req, res) => {
+    var data = await loggerProxy.get(`v1/articles?source=bbc-news`);
+    res.send(data);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/news/:id', async (req, res) => {
+    var data = await loggerProxy.get(`v1/articles?source=${req.params.id}`);
+    res.send(data);
+});
+
+app.post('/news', async (req, res) {
+    res.send('POST request to the news')
+});
+
+app.put('/news/:id', async (req, res) {
+    res.send('PUT request to the news/id')
+});
+
+app.delete('/news/:id', async (req, res) {
+    res.send('DELETE request to the news/id')
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
