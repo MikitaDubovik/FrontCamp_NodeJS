a3d53f05da83b58395c6d2d3367d94a6fc07309d
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-11-23 18:37:55 +0300

    Task 4

--- scripts/logger-proxy/logger-proxy.js
+++ scripts/logger-proxy/logger-proxy.js
@@ -10,7 +10,8 @@ class LoggerProxy {
                     level: 'info',
                     format: winston.format.json(),
                     transports: [
-                        new winston.transports.Console()
+                        new winston.transports.Console(),
+                        new winston.transports.File({ filename: 'combined.log' })
                     ]
                 });
 
--- server.js
+++ server.js
@@ -11,21 +11,42 @@ app.get('/news', async (req, res) => {
     res.send(data);
 });
 
-app.get('/news/:id', async (req, res) => {
-    var data = await loggerProxy.get(`v1/articles?source=${req.params.id}`);
-    res.send(data);
+app.get('/news/:id', async (req, res, next) => {
+    try {
+        var data = await loggerProxy.get(`v1/articles?source=${req.params.id}`);
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.post('/news', async (req, res) {
-    res.send('POST request to the news')
+app.post('/news', async (req, res) => {
+    try {
+        res.send('POST request to the news')
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.put('/news/:id', async (req, res) {
-    res.send('PUT request to the news/id')
+app.put('/news/:id', async (req, res) => {
+    try {
+        res.send('PUT request to the news/id')
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.delete('/news/:id', async (req, res) {
-    res.send('DELETE request to the news/id')
+app.delete('/news/:id', async (req, res) => {
+    try {
+        res.send('DELETE request to the news/id')
+    } catch (err) {
+        next(err);
+    }
 });
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send(`Something broke! - ${err.message}`);
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
